08291e5774cc18999b36feb348d9c4b77f662be6
Jamir Peroba <jamir.peroba@pelotas.com.br>
2018-01-22 17:33:02 -0200

    One to many

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,15 @@
 
 //ONTE TO ONE
 
- $this->get('one-to-one','OneToOneController@oneToOne');
- $this->get('one-to-one-inverse','OneToOneController@oneToOneInverse');
+$this->get('one-to-one','OneToOneController@oneToOne');
+$this->get('one-to-one-inverse','OneToOneController@oneToOneInverse');
 $this->get('one-to-one-insert','OneToOneController@oneToOneInsert');
+
+//One to Many 
+$this->get('one-to-many', 'OneToManyController@oneToMany');
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
