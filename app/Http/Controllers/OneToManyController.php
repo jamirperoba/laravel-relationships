b23f41c5acb8e547692fa15474a87784d957349b
jamir peroba <perobajamir@gmail.com>
2018-01-24 00:15:58 -0200

    Relacionamenteo One To Many

--- app/Http/Controllers/OneToManyController.php
+++ app/Http/Controllers/OneToManyController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\State;
+use App\Models\Country;
 
 class OneToManyController extends Controller
 {
-    //
+	public function oneToMany(){
+    	// $country = Country::where('name','Brazil')->get()->first();
+		$keySearch = 'Bra';
+		$countries = Country::where('name','LIKE', "%{$keySearch}%")->get();
+		foreach ($countries as $country) {
+			echo "<b>{$country->name}</b>";
+			// echo $country->name;
+
+    	// $states = $country->states()->where('initials','RS')->get();
+			$states = $country->states;
+
+			foreach($states as $state){
+				echo "<br> {$state->initials} - {$state->name}";
+			}
+			echo "<hr>";
+		}
+	}
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Location;
+use App\Models\State;
 
 class Country extends Model
 {
@@ -11,5 +13,8 @@ class Country extends Model
     public function location(){
     	return $this->hasOne(Location::class);
     	// return $this->hasOne(Location::class, 'country_id','id');
+    }
+    public function states(){
+    	return $this->hasMany(State::class);
     }
 }
